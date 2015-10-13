2c53599d0743af12eba262098ae3b351949bfa07
Andreas Küpper <andreas.kupper@yale.edu>
2015-10-12 20:30:43 -0400

    first working version

--- extract_stats.py
+++ extract_stats.py
@@ -3,24 +3,90 @@
 import sys
 import re
 
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from cStringIO import StringIO
+
+
 """Some statistics on the German educational system.
 Extracted from pdf files from the DAAD website:
 https://www.daad.de/der-daad/zahlen-und-fakten/de/29877-daad-bundeslaenderstatistiken
 """
 
 
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,password=password,caching=caching, check_extractable=True):
+
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+  
 def main():
   args = sys.argv[1:]
 
   if not args:
-    print 'usage: python germanstats.py file [file ...]'
+    print 'usage: python extract_stats.py file [file ...]'
     sys.exit(1)
+
+  f = open('summary.txt', 'w')
 
   # For each filename, get the stats
   for filename in args:
       print filename
-      f = open(filename+'.summary.txt', 'w')
-      f.close()
+      text_org = convert_pdf_to_txt(filename)
+      temp = re.search('([\w.-]+)\s+Deutschland', text_org)
+      state = temp.group(1)
+      print state
+      text_wo_letters = re.sub(r'[a-zA-Z]', r'', text_org)
+      text_wo_perc = re.sub(r'%', r'', text_wo_letters)
+      text = re.sub(r'[0-9]+,[0-9]+', r'', text_wo_perc)
+      temp = re.search('([\w.-]+)\s+80.523.800', text)
+      inhabitants = temp.group(1)
+      temp = re.search('([\w.-]+)\s+2.399.409', text)
+      students = temp.group(1)
+      temp = re.search('([\w.-]+)\s+495.088', text)
+      freshmen = temp.group(1)
+      temp = re.search('([\w.-]+)\s+413.338', text)
+      degrees = temp.group(1)
+      temp = re.search('([\w.-]+)\s+353.690', text)
+      employees = temp.group(1)
+      temp = re.search('([\w.-]+)\s+43.862', text)
+      professors = temp.group(1)
+      temp = re.search('([\w.-]+)\s+204.644', text)
+      foreign_students = temp.group(1)
+      temp = re.search('([\w.-]+)\s+79.537', text)
+      foreign_freshmen = temp.group(1)
+      temp = re.search('([\w.-]+)\s+30.806', text)
+      foreign_degrees = temp.group(1)
+      temp = re.search('([\w.-]+)\s+35.345', text)
+      foreign_employees = temp.group(1)
+      temp = re.search('([\w.-]+)\s+2.778', text)
+      foreign_professors = temp.group(1)
+      
+      print state, inhabitants, students, freshmen, degrees, employees, professors, foreign_students, foreign_freshmen, foreign_degrees, foreign_employees, foreign_professors
+      f.write(state+'\t'+inhabitants+'\t'+students+'\t'+freshmen+'\t'+degrees+'\t'+employees+'\t'+professors+'\t'+foreign_students+'\t'+foreign_freshmen+'\t'+foreign_degrees+'\t'+foreign_employees+'\t'+foreign_professors+'\n')
+
+  f.close()
 
 
 
