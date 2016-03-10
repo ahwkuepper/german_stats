0a87f0042c0c1a727d7de1592394d001823f2656
Andreas Küpper <andreas.kupper@yale.edu>
2016-03-10 14:32:23 -0500

    added some comments

--- extract_stats.py
+++ extract_stats.py
@@ -11,11 +11,11 @@ from cStringIO import StringIO
 
 
 """Some statistics on the German educational system.
-Extracted from pdf files from the DAAD website:
+Extracted from PDF files from the DAAD website:
 https://www.daad.de/der-daad/zahlen-und-fakten/de/29877-daad-bundeslaenderstatistiken
 """
 
-
+#PDF text extractor
 def convert_pdf_to_txt(path):
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
@@ -50,17 +50,28 @@ def main():
 
   f = open('summary.txt', 'w')
 
-  # For each filename, get the stats
+  # For each filename get the stats
   for filename in args:
-      print filename
+
+      #grab text from PDF file
       text_org = convert_pdf_to_txt(filename)
+
+      #grab name of Federal State from PDF header
       temp = re.search('([\w.-]+)\s+Deutschland', text_org)
       state = temp.group(1)
       print state
+
+      #remove letters
       text_wo_letters = re.sub(r'[a-zA-Z]', r'', text_org)
+      
+      #remove percent symbols
       text_wo_perc = re.sub(r'%', r'', text_wo_letters)
+      
+      #convert German number format to US format
       text_wo_frac = re.sub(r'[0-9]+,[0-9]+', r'', text_wo_perc)
       text = re.sub(r'\.', r'', text_wo_frac)
+      
+      #extract the quantities
       temp = re.search('([\w.-]+)\s+80523800', text)
       inhabitants = temp.group(1)
       temp = re.search('([\w.-]+)\s+2399409', text)
@@ -84,6 +95,7 @@ def main():
       temp = re.search('([\w.-]+)\s+2778', text)
       foreign_professors = temp.group(1)
       
+      #write to stdout and file
       print state, inhabitants, students, freshmen, degrees, employees, professors, foreign_students, foreign_freshmen, foreign_degrees, foreign_employees, foreign_professors
       f.write(state+'\t'+inhabitants+'\t'+students+'\t'+freshmen+'\t'+degrees+'\t'+employees+'\t'+professors+'\t'+foreign_students+'\t'+foreign_freshmen+'\t'+foreign_degrees+'\t'+foreign_employees+'\t'+foreign_professors+'\n')
 
